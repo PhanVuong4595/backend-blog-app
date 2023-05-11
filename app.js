bcaec22df3c85b658b10ecfe8425cc43e040b695
Phan Vuong <pdvuongtb@gmail.com>
2023-05-11 22:33:32 +0700

    setup

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import mongoose from 'mongoose';
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
 
 // middleware
 app.use(cors());
-app.use(morgan('tiny'));
-app.use(express.json())
+app.use(morgan("tiny"));
+app.use(express.json());
 
 // connnect database
-mongoose.connect('mongodb://localhost:27017/baithi')
-    .then(() => console.log("Kết nối db thành công"))
-    .catch((error) => console.log(error));
-    
+mongoose
+  .connect("mongodb://localhost:27017/baithi")
+  .then(() => console.log("Kết nối db thành công"))
+  .catch((error) => console.log(error));
+
 // connection
 const PORT = 8000;
 app.listen(PORT, () => {
-    console.log("Server is running port", PORT);
-})
+  console.log("Server is running port", PORT);
+});
